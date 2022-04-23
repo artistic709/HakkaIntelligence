1b8bf8620ed2028e36b8f1ba9100df655b1e25b1
artistic709 <artistic709@gmail.com>
2022-04-24 04:51:21 +0800

    reuseable keeper, will proceed if required

--- HIKeeper.sol
+++ HIKeeper.sol
@@ -6,16 +6,18 @@ interface IHakkaIntelligence {
     function reveal(address _player) external returns (uint256 score);
     function revealOpen() external view returns (uint256);
     function revealClose() external view returns (uint256);
+    function periodStop() external view returns (uint256);
+    function proceed() external;
 }
 
 contract HakkaIntelligenceMock {
     uint256 internal ro;
     uint256 internal rc;
+    uint256 internal ps;
     mapping(address => bool) public revealed;
 
     constructor() {
-        ro = block.timestamp + 600;
-        rc = block.timestamp + 1800;
+        ps = block.timestamp + 60;
     }
 
     function reveal(address _player) external returns (uint256 score) {
@@ -23,6 +25,14 @@ contract HakkaIntelligenceMock {
         require(!revealed[_player]);
         revealed[_player] = true;
         return 1;
+    }
+    function proceed() external {
+        require(block.timestamp > ps);
+        ro = block.timestamp + 300;
+        rc = block.timestamp + 1800;
+    }
+    function periodStop() external view returns (uint256) {
+        return ps;
     }
     function revealOpen() external view returns (uint256) {
         return ro;
@@ -39,14 +49,25 @@ contract keeper {
     uint256 public index;
     address public owner;
     address[] public queue;
+    uint256 public flag;
 
     event Register(address indexed user);
     event Perform(address indexed user);
+    event Init(address HI, uint256 cost);
 
-    constructor(address _HI, uint256 _cost) {
+    constructor() {
+        owner = msg.sender;
+    }
+
+    function init(address _HI, uint256 _cost) external {
+        require(msg.sender == owner, "not owner");
+        require(queue.length == index, "Work not done yet");
         HI = IHakkaIntelligence(_HI);
         cost = _cost;
-        owner = msg.sender;
+        index = 0;
+        delete queue;
+        flag = 1;
+        emit Init(_HI, _cost);
     }
 
     function getQueueLength() external view returns (uint256) {
@@ -60,7 +81,11 @@ contract keeper {
     }
 
     function validate() internal view returns (bool) {
-        return HI.revealOpen() < block.timestamp && HI.revealClose() > block.timestamp && queue.length > index;
+        if(flag == 2)
+            return HI.revealOpen() < block.timestamp && HI.revealClose() > block.timestamp && queue.length > index;
+        else if(flag == 1)
+            return HI.periodStop() >= block.timestamp;
+        else return false;
     }
 
     function checkUpkeep(bytes calldata) external view returns (bool upkeepNeeded, bytes memory performData) {
@@ -68,12 +93,19 @@ contract keeper {
     }
 
     function performUpkeep(bytes calldata) external {
-        require(validate(), "invalid");
-        address target = queue[index];
-        bytes memory data = abi.encodeWithSelector(HI.reveal.selector, target);
-        (bool success, ) = address(HI).call(data);
-        if ( success) emit Perform(target);
-        ++index;
+        require(validate(), "invalid upkeep");
+
+        if(flag == 1) {
+            if(HI.revealOpen() == 0) HI.proceed();
+            flag = 2;
+        }
+        else {
+            address target = queue[index];
+            bytes memory data = abi.encodeWithSelector(HI.reveal.selector, target);
+            (bool success, ) = address(HI).call(data);
+            if ( success) emit Perform(target);
+            ++index;
+        }
     }
 
     function withdraw() external {
