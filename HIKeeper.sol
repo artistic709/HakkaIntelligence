3cffb1f32dbff1a82b727aaecb3f9427618c8d87
artistic709 <artistic709@gmail.com>
2022-04-27 18:07:29 +0800

    automate proceed() when periodStart

--- HIKeeper.sol
+++ HIKeeper.sol
@@ -7,17 +7,22 @@ interface IHakkaIntelligence {
     function revealOpen() external view returns (uint256);
     function revealClose() external view returns (uint256);
     function periodStop() external view returns (uint256);
+    function periodStart() external view returns (uint256);
     function proceed() external;
 }
 
 contract HakkaIntelligenceMock {
     uint256 internal ro;
     uint256 internal rc;
-    uint256 internal ps;
+    uint256 internal pst;
+    uint256 internal psp;
+    uint256 internal flag;
     mapping(address => bool) public revealed;
 
     constructor() {
-        ps = block.timestamp + 60;
+        pst = block.timestamp + 60;
+        psp = block.timestamp + 120;
+        flag = 1;
     }
 
     function reveal(address _player) external returns (uint256 score) {
@@ -27,12 +32,22 @@ contract HakkaIntelligenceMock {
         return 1;
     }
     function proceed() external {
-        require(block.timestamp > ps);
-        ro = block.timestamp + 300;
-        rc = block.timestamp + 1800;
+        if(flag == 1) {
+            require(block.timestamp > pst);
+            flag = 2;
+        }
+        else if(flag == 2){
+            require(block.timestamp > psp);
+            ro = block.timestamp + 300;
+            rc = block.timestamp + 1800;
+            flag = 3;
+        }
+    }
+    function periodStart() external view returns (uint256) {
+        return pst;
     }
     function periodStop() external view returns (uint256) {
-        return ps;
+        return psp;
     }
     function revealOpen() external view returns (uint256) {
         return ro;
@@ -54,6 +69,7 @@ contract keeper {
     event Register(address indexed user);
     event Perform(address indexed user);
     event Init(address HI, uint256 cost);
+    event Proceed(address indexed HI);
 
     constructor() {
         owner = msg.sender;
@@ -81,10 +97,12 @@ contract keeper {
     }
 
     function validate() internal view returns (bool) {
-        if(flag == 2)
+        if(flag == 3)
             return HI.revealOpen() < block.timestamp && HI.revealClose() > block.timestamp && queue.length > index;
+        else if(flag == 2)
+            return HI.periodStop() <= block.timestamp;
         else if(flag == 1)
-            return HI.periodStop() >= block.timestamp;
+            return HI.periodStart() <= block.timestamp;
         else return false;
     }
 
@@ -95,16 +113,24 @@ contract keeper {
     function performUpkeep(bytes calldata) external {
         require(validate(), "invalid upkeep");
 
-        if(flag == 1) {
-            if(HI.revealOpen() == 0) HI.proceed();
-            flag = 2;
-        }
-        else {
+        if(flag == 3) {
             address target = queue[index];
             bytes memory data = abi.encodeWithSelector(HI.reveal.selector, target);
             (bool success, ) = address(HI).call(data);
-            if ( success) emit Perform(target);
+            if (success) emit Perform(target);
             ++index;
+        }
+        else if(flag == 2) {
+            bytes memory data = abi.encodeWithSelector(HI.proceed.selector);
+            (bool success, ) = address(HI).call(data);
+            if(success) emit Proceed(address(HI));
+            flag = 3;
+        }
+        else if(flag == 1) {
+            bytes memory data = abi.encodeWithSelector(HI.proceed.selector);
+            (bool success, ) = address(HI).call(data);
+            if(success) emit Proceed(address(HI));
+            flag = 2;
         }
     }
 
